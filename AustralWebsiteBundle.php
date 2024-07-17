5fe28670536647f31975c96db2df78e7941e4e69
Matthieu Beurel <matthieu@mbeurel.com>
2024-07-17 09:23:58 +0200

    feat : Add compatibility Symfony 6.4

--- AustralWebsiteBundle.php
+++ AustralWebsiteBundle.php
@@ -24,7 +24,7 @@ class AustralWebsiteBundle extends Bundle
   /**
    * @param ContainerBuilder $container
    */
-  public function build(ContainerBuilder $container)
+  public function build(ContainerBuilder $container): void
   {
     parent::build($container);
     $container->addCompilerPass(new DoctrineResolveTargetEntityPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 1000);
